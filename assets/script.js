53ee0ff4da31792cc43ec2ea93fdfe4354285392
dboren <danielboren@att.net>
2021-02-01 21:20:12 -0600

    added event listener to search button. Need to modify fetch function because it is encountering a CORS error

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,14 @@
-
+var searchBtn = document.getElementById("search-btn");
+var cityInput = document.getElementById("city-search");
 
 //inital fetch function below
 
 function getOrgs(searchedCity) {
-    var proPubUrl = "https://projects.propublica.org/nonprofits/api/v2/search.json?q=" + searchedCity + "&ntee%5Bid%5D=3";
+    var proPubUrl = "https://projects.propublica.org/nonprofits/api/v2/search.json?q=" + cityInput.value + "&ntee%5Bid%5D=3";
     //url format is [API base request URL] + [search text entered by user] + [NTEE code for animal and environment orgs]
     //variable names subject to change
 
-    fetch(areaOrgs)
+    fetch(proPubUrl)
         .then(function (response) {
             if (response.ok) {
                 response.json()
@@ -23,3 +24,5 @@ function getOrgs(searchedCity) {
             alert("Unable to connect to ProPublica NonProfit Explorer");
         });
 };
+
+searchBtn.addEventListener("click", getOrgs);
