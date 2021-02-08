ab00f962068448f03a8dec0b5836885a8e124cbd
amcourson <amcourson09@sbcglobal.net>
2021-02-07 21:19:08 -0600

    adding modal

--- assets/script.js
+++ assets/script.js
@@ -288,3 +288,54 @@ geojson.features.forEach(function(marker) {
 // query: (2)["los", "angeles"]
 // type: "FeatureCollection"
 // __proto__: Object
+
+
+
+
+// document.addEventListener('DOMContentLoaded', function () {
+
+//   // Modals
+
+//   var rootEl = document.documentElement;
+//   var allModals = getAll('.modal');
+//   var modalButtons = getAll('.modal-button');
+//   var modalCloses = getAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button');
+
+//   if (modalButtons.length > 0) {
+//       modalButtons.forEach(function (el) {
+//           el.addEventListener('click', function () {
+//               var target = document.getElementById(el.dataset.target);
+//               rootEl.classList.add('is-clipped');
+//               target.classList.add('is-active');
+//           });
+//       });
+//   }
+
+//   if (modalCloses.length > 0) {
+//       modalCloses.forEach(function (el) {
+//           el.addEventListener('click', function () {
+//               closeModals();
+//           });
+//       });
+//   }
+
+//   document.addEventListener('keydown', function (event) {
+//       var e = event || window.event;
+//       if (e.keyCode === 27) {
+//           closeModals();
+//       }
+//   });
+
+//   function closeModals() {
+//       rootEl.classList.remove('is-clipped');
+//       allModals.forEach(function (el) {
+//           el.classList.remove('is-active');
+//       });
+//   }
+
+//   // Functions
+
+//   function getAll(selector) {
+//       return Array.prototype.slice.call(document.querySelectorAll(selector), 0);
+//   }
+// });
